6d6f57580c186f2e530dec8d08dd610a437ecaa7
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-10-14 18:07:42 +0200

    set-up starting html and css

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/tic tac toe/i);
   expect(linkElement).toBeInTheDocument();
 });
