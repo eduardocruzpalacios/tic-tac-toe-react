55735532f04f4b7c289e84be35da9e890ef3764a
eduardocruzpalacios <52535457+eduardocruzpalacios@users.noreply.github.com>
2022-10-23 23:40:33 +0200

    fix test

--- src/components/organisms/Game/Game.test.tsx
+++ src/components/organisms/Game/Game.test.tsx
@@ -14,14 +14,14 @@ describe('<Game />', () => {
     for (let i = 0; i < 4; i++) {
       fireEvent.click(buttonEls[i]);
     }
-    let tiles1Els = screen.getAllByText(/o/i);
-    let tiles2Els = screen.getAllByText(/x/i);
+    let tiles1Els = screen.getAllByText(/^o$/i);
+    let tiles2Els = screen.getAllByText(/^x$/i);
     expect(tiles1Els).toHaveLength(2);
     expect(tiles2Els).toHaveLength(2);
     const resetButtonEl = screen.getByText(/reset/i);
     fireEvent.click(resetButtonEl);
-    tiles1Els = screen.queryAllByText(/o/i);
-    tiles2Els = screen.queryAllByText(/x/i);
+    tiles1Els = screen.queryAllByText(/^o$/i);
+    tiles2Els = screen.queryAllByText(/^x$/i);
     expect(tiles1Els).toHaveLength(0);
     expect(tiles2Els).toHaveLength(0);
   });
